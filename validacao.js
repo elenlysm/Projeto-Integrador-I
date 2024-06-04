50a457aad518663f4dfaaa4552bcf40b9a0cef88
P3rb4s <lucasperboni33@gmail.com>
2024-06-04 20:08:41 -0300

    slaporra

--- validacao.js
+++ validacao.js
@@ -1,47 +1,45 @@
-const usuarios =[
+const usuarios = [
     {
-        login:'perboni@gmail.com',
-        pass:'perbas'
+        login: 'perboni@gmail.com',
+        senha: 'perbas'
     },
     {
-        login:'elen',
-        pass:'freire'
+        login: 'elenf@gmail.com',
+        senha: 'freire'
     },
     {
-        login:'bia',
-        pass:'melhado'
+        login: 'biam@gmail.com',
+        senha: 'melhado'
     },
     {
-        login: 'bia',
-        pass: 'patinho'
+        login: 'biap@gmail.com',
+        senha: 'patinho'
     }
 ]
 
 let botao = document.getElementById('btn')
 
-botao.addEventListener('click', function logar(){
+botao.addEventListener('click', function logar() {
     var login = document.getElementById('user').value;
-    var senha= document.getElementById('senha').value;
+    var senha = document.getElementById('senha').value;
     var validalogin = false
 
-for(let i in usuarios){
+    for (let i in usuarios) {
+        if (login == usuarios[i].login && senha == usuarios[i].senha) {
+            validalogin = true
+            location.href = "D:\Faculdade\Projeto Integrador\Projeto-Integrador-I\reservas.html";
 
-
-    if(login == usuarios[i].login && senha == usuarios[i].pass){
-        validalogin=true
-        location.href= "reservas.html";
-        break
+        }
     }
-}
-if(validalogin==true){
-    alert('OK')
-}else{
-      alert('ERRO')
-}
+    if (validalogin == true) {
+        alert('OK')
+    } else {
+        alert('ERRO')
+    }
 })
 
 
-window.addEventListener("scroll",function(){
+window.addEventListener("scroll", function () {
     let header = document.querySelector('#header')
-    header.classList.toggle('rolagem',window.scrollY > 120);
+    header.classList.toggle('rolagem', window.scrollY > 120);
 })
